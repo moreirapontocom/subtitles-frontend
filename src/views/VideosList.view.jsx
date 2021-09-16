c5611bb9c7379074a8ed202880636d8be0436f6b
Lucas Moreira <moreirapontocom@gmail.com>
2021-09-16 09:25:55 -0300

    More layout changes

--- src/views/VideosList.view.jsx
+++ src/views/VideosList.view.jsx
@@ -1,6 +1,7 @@
 import Panel from "../views/Panel.view";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "flag-icon-css/css/flag-icon.min.css";
 
 const VideosListView = () => {
   const [videos] = useState(getVideos());
@@ -15,10 +16,10 @@ const VideosListView = () => {
         url: "https://www.youtube.com/watch?v=2_FJrmft3uQ",
         duration: "1:30",
         lang: {
-          original: "PT",
-          target: "EN",
+          original: "br",
+          target: "us",
         },
-        status: "completed",
+        status: "not_started",
         startedAt: "2020-01-01 10:12:25",
         updatedAt: "2020-01-05 23:00:12",
       },
@@ -30,8 +31,8 @@ const VideosListView = () => {
         url: "https://www.youtube.com/watch?v=EvJR-uBrPpo",
         duration: "1:30",
         lang: {
-          original: "PT",
-          target: "PT",
+          original: "br",
+          target: "br",
         },
         status: "completed",
         startedAt: "2020-01-01 10:12:25",
@@ -39,28 +40,57 @@ const VideosListView = () => {
       },
     ];
 
+    function getJobStatusDescription(jobStatus) {
+      switch (jobStatus) {
+        case "completed":
+          return <span className="badge bg-success">Complete</span>;
+
+        case "not_started":
+          return <span className="badge bg-secondary">To Do</span>;
+
+        case "in_progress":
+          return <span className="badge bg-primary">In Progress</span>;
+
+        default:
+          return <span className="badge bg-primary">--</span>;
+      }
+    }
+
     return list.map((video) => {
       return (
         <tr key={video.url}>
-          <td className="text-center">
-            {video.status === "completed" ? (<i className="fas fa-clock"></i>) : (<i className="fas fa-check"></i>)}
-          </td>
           <td>
             <img src={video.cover} alt={video.titulo} height="70" />
           </td>
           <td>
             <div>
-              <Link to={`/videos/${video.id}`}>{video.titulo}</Link>
+              <strong>{video.titulo}</strong>
             </div>
-            {video.description}
-          </td>
-          <td>
-            {(video.lang.original === video.lang.target) ? 'Legendar' : 'Traduzir'} ({video.lang.original} &gt; {video.lang.target})
-          </td>
-          <td>
+            <div>
+              {video.description}
+            </div>
             <a href={video.url} target="_blank" rel="noreferrer">
               {video.url}
             </a>
+          </td>
+          <td>
+            {video.lang.original === video.lang.target
+              ? "Legendar"
+              : "Traduzir"}
+          </td>
+          <td className="text-center">
+            <span
+              className={`flag-icon flag-icon-${video.lang.original}`}
+            ></span>
+          </td>
+          <td className="text-center">
+            <span className={`flag-icon flag-icon-${video.lang.target}`}></span>
+          </td>
+          <td>{getJobStatusDescription(video.status)}</td>
+          <td>
+            <Link to={`/videos/${video.id}`} className="btn btn-secondary">
+              <i className="fas fa-keyboard"></i>
+            </Link>
           </td>
         </tr>
       );
@@ -76,11 +106,13 @@ const VideosListView = () => {
         <table className="table table-striped">
           <thead>
             <tr>
-              <th className="text-center">Status</th>
               <th>Cover</th>
               <th>Titulo</th>
               <th>Job</th>
-              <th>URL</th>
+              <th className="text-center">Original</th>
+              <th className="text-center">Destino</th>
+              <th>Status</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>{videos}</tbody>
